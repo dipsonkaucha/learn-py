21b09292b0daca1858a2ec492b5fabc272612d72
dipsonkaucha <madipsonkaucha@gmail.com>
2026-03-27 00:13:42 +0545

    learn python datatypes

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+# Datatypes provided by python 
+
+# 1. Text Type:	
+    # str = "Python is Easy!"
+
+# 2. Numeric Types:
+    # int num = 67
+    # float flt = 67.69
+    # complex cmp = 6 + 7j OR cmp = complex(6,7)
+
+# 3. Sequence Types:	
+    # list li = ["apple", "banana", "cherry"]
+    # tuple tup = ("apple", "banana", "cherry")
+    # range x = range(n) it generates numbers in that range of n-1
+
+# 4. Mapping Type:	
+    # dict x = {"name" : "John", "age" : 36}   // must be key: value
+    
+# 5. Set Types:	
+    # set x = {"apple", "banana", "cherry"}   //we can add/modify items
+    # frozenset x = frozenset({"apple", "banana", "cherry"})   // we can't add/modify items
+    
+# 6. Boolean Type:	
+    # bool var = True OR False
+
+# 7. Binary Types:	
+    # bytes x = b"Hello"
+    # bytearray x = bytearray(5)
+    # memoryview x = memoryview(bytes(5))
+
+# 8. None Type:	
+    # NoneType x = None
+
+
